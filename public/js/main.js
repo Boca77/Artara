e2c591ddc7a318f05f4fb6f902e1adf82d8e1618
Boris Nacev <bnachev77@gmail.com>
2025-05-03 20:21:47 +0200

    product proccessing changed

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,28 @@
 $(document).ready(function() {
+    let allProducts = [];
+    $.ajax({
+        url: 'assets/products.json',
+        method: 'GET',
+            success: function(response) {
+            allProducts = response;
+        }
+    })
     
     $('.btn-picture').click(function() {
         let id = $(this).data('id');
         $('.single-page').removeClass('d-none');
-        $.ajax({
-            url: 'assets/products.json',
-            method: 'GET',
-             success: function(response) {
-                const productId = id; 
-                const product = response.products.find(p => p.id === productId);
 
-                $('#product-name').text(product.name);
-                $('#product-price').text('$' + product.price);
-                $('#product-description').text(product.description);
-                $('#main-product-image').attr('src', 'assets/placeholder-imgs/' + product.image);
+        const product = allProducts.products.find(p => p.id === id);
 
-                const moreImages = product['more-imgs'];
-                const moreImagesHtml = moreImages.map(img => `<img src="assets/placeholder-imgs/${img}" alt="">`).join('');
-                $('#more-images').html(moreImagesHtml);
+        $('#product-name').text(product.name);
+        $('#product-price').text('$' + product.price);
+        $('#product-description').text(product.description);
+        $('#main-product-image').attr('src', 'assets/placeholder-imgs/' + product.image);
 
-                if (product) {
-                    console.log(product);
-                } else {
-                    console.log('Product not found');
-                }
-            }
-        })
+        const moreImages = product['more-imgs'];
+        const moreImagesHtml = moreImages.map(img => `<img src="assets/placeholder-imgs/${img}" alt="">`).join('');
+        $('#more-images').html(moreImagesHtml);
+       
     });
-
    
 });
